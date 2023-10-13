e288fc5c6ed0e85bd62759a5c6d5ccd85f100e97
gioboa <giorgiob.boa@gmail.com>
2023-10-13 13:02:17 +0200

    feat: add loadComponentGraph function

--- packages/devtools/src/lib/DevTools.tsx
+++ packages/devtools/src/lib/DevTools.tsx
@@ -1,5 +1,10 @@
-import { component$ } from '@builder.io/qwik';
+import { component$, useVisibleTask$ } from '@builder.io/qwik';
+import { loadComponentGraph } from '../utils';
 
 export const DevTools = component$(() => {
+  useVisibleTask$(() => {
+    const cmpGraph = loadComponentGraph();
+    console.log('cmpGraph', cmpGraph);
+  });
   return <div>Qwik DevTools</div>;
 });
---
+++ packages/devtools/src/utils/index.ts
@@ -0,0 +1,7 @@
+import { $ } from '@builder.io/qwik';
+
+export const loadComponentGraph = () => {
+  // const walker = document.createTreeWalker(document.body, NodeFilter.SHOW_ALL);
+  // const node = walker.firstChild();
+  return {};
+};
