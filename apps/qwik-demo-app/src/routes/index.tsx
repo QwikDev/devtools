772fef8f950711e1afc033b9ab208ed910bbaf40
gioboa <giorgiob.boa@gmail.com>
2023-10-13 10:53:30 +0200

    intial commit 🚀

---
+++ apps/qwik-demo-app/src/routes/index.tsx
@@ -0,0 +1,6 @@
+import { component$ } from '@builder.io/qwik';
+import { DevTools } from 'qwikdev/devtools';
+
+export default component$(() => {
+	return <DevTools />;
+});
---
+++ packages/devtools/src/lib/DevTools.tsx
@@ -0,0 +1,5 @@
+import { component$ } from '@builder.io/qwik';
+
+export const DevTools = component$(() => {
+  return <div>Qwik DevTools</div>;
+});
