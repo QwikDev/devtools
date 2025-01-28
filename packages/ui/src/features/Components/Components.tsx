ee24172cf306f91d1929fda1d7365bdf0bb85e64
Damián Pumar <damianpumar@gmail.com>
2025-01-28 09:13:28 +0100

    feat: Improve update state

--- packages/ui/src/features/Components/Components.tsx
+++ packages/ui/src/features/Components/Components.tsx
@@ -1,9 +1,39 @@
-//@ts-nocheck
-
-import { component$, useStyles$ } from "@qwik.dev/core";
+import {
+  $,
+  component$,
+  QRL,
+  useSignal,
+  useStore,
+  useStyles$,
+  useVisibleTask$,
+} from "@qwik.dev/core";
 import { QGreetings } from "./components/Tree";
 import reactTreeCss from "react-complex-tree/lib/style-modern.css?inline";
 import styles from "./styles.css?inline";
+
+// {"components/Button/Button.tsx":{"path":"components/Button/Button.tsx","state":{"store":{"value":{"count":0},"type":"store"}}}
+
+export interface ComponentStateStore {
+  [key: string]: {
+    type: "signal" | "store";
+    value: any;
+  };
+}
+
+export interface ComponentState {
+  path: string;
+  state: ComponentStateStore;
+  type: string;
+}
+
+declare global {
+  interface Window {
+    __QWIK_DEVTOOLS__: {
+      appState: Record<string, ComponentState>;
+    };
+  }
+}
+
 export const Components = component$(() => {
   useStyles$(reactTreeCss);
   useStyles$(styles);
@@ -11,26 +41,91 @@ export const Components = component$(() => {
   return (
     <div>
       <QGreetings />
-      {/* <div>
-        Count signal value:{" "}
-        {
-          window.__QWIK_DEVTOOLS__.appState["routes/index.tsx"].state.count
-            .value.value
-        }
-      </div>
-      <div>Increase count:</div>
-      <button
-        class="rounded-md bg-blue-500 px-2 py-1 text-white"
-        onClick$={() => {
-          window.__QWIK_DEVTOOLS__.appState[
-            "routes/index.tsx"
-          ].state.count.value.value =
-            window.__QWIK_DEVTOOLS__.appState["routes/index.tsx"].state.count
-              .value.value + 1;
-        }}
+
+      <ComponentsStore />
+    </div>
+  );
+});
+
+const ComponentsStore = component$(() => {
+  const selectedComponent = useSignal<string | null>(null);
+  const components = window.__QWIK_DEVTOOLS__.appState;
+  const componentStore = Object.keys(components);
+
+  const handleComponentSelected = $((key: string) => {
+    selectedComponent.value = key;
+  });
+
+  return (
+    <div>
+      <select
+        class="mt-2 w-1/2 rounded-md border bg-transparent p-2 font-mono"
+        onChange$={(_, e) => handleComponentSelected(e.value)}
       >
-        +1
-      </button> */}
+        <option value="">Select component</option>
+        {componentStore.map((key) => (
+          <option value={key} class="p-2">
+            {key}
+          </option>
+        ))}
+      </select>
+
+      {selectedComponent.value && (
+        <ComponentStoreUpdate
+          value={components[selectedComponent.value].state}
+        />
+      )}
+    </div>
+  );
+});
+
+const ComponentStoreUpdate = component$<{
+  value: ComponentStateStore;
+}>(({ value }) => {
+  return (
+    <div class="mt-2 flex  w-1/2 flex-col font-mono text-white">
+      {Object.keys(value).map((key) => {
+        if (value[key].type === "signal") {
+          return (
+            <div class="flex flex-col">
+              <h2 class="text-lg font-bold">Signal: {key}</h2>
+              <input
+                class="mt-2 rounded-md border bg-transparent p-2 font-mono"
+                type={typeof value[key].value === "string" ? "text" : "number"}
+                bind:value={value[key].value}
+              />
+            </div>
+          );
+        }
+
+        if (value[key].type === "store") {
+          return (
+            <div class="flex flex-col">
+              <h2 class="text-lg font-bold">Store: {key}</h2>
+              {Object.keys(value[key].value).map((storeKey) => (
+                <div class="flex flex-col">
+                  <h3 class="text-md font-bold">Key: {storeKey}</h3>
+                  <input
+                    class="mt-2 rounded-md border bg-transparent p-2 font-mono"
+                    type={
+                      typeof value[key].value[storeKey] === "string"
+                        ? "text"
+                        : "number"
+                    }
+                    value={value[key].value[storeKey]}
+                    onChange$={(_, e) => {
+                      value[key].value[storeKey] =
+                        typeof value[key].value[storeKey] === "string"
+                          ? e.value
+                          : Number(e.value);
+                    }}
+                  />
+                </div>
+              ))}
+            </div>
+          );
+        }
+      })}
     </div>
   );
 });
