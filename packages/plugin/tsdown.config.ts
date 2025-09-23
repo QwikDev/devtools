e5cadc55e9591e2e26c649a8f2bb37d5f8647163
Jerry_Wu <409187100@qq.com>
2025-09-23 14:04:22 +0800

    fix cannot be as plugin and cannot be imported

--- packages/plugin/tsdown.config.ts
+++ packages/plugin/tsdown.config.ts
@@ -3,8 +3,9 @@ import { defineConfig } from 'tsdown'
 export default defineConfig({
   entry: ['src/index.ts'],
   clean: true,
+  target: 'esnext',
   format: ['esm'],
-  external: ['vite','vite-plugin-inspect'],
+  external: ['vite','vite-plugin-inspect', '@qwik.dev/core'],
   dts: true,
   shims: true,
   tsconfig: './tsconfig.json',
